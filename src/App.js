560176c4448138f6589307e8b0751e9205b5153c
zhangtong <1263695353@qq.com>
2018-06-09 10:32:56 +0800

    use native-base

--- src/App.js
+++ src/App.js
@@ -11,42 +11,50 @@ import {
   Text,
   View
 } from 'react-native';
-// import {Container, Footer, FooterTab, Header, Body} from 'native-base';
+import {Container, Footer, FooterTab, Header, Body} from 'native-base';
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
+      <Container style={styles.container}>
+        <Header>
+          <Text style={styles.welcome}>
           Welcome to React  Native!
         </Text>
+      </Header>
+      <Body>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
         </Text>
-        <Text style={styles.instructions}>
-            Press Cmd+R to reload,{'\n'}
-            Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      </Body>
+      <Footer>
+        <FooterTab>
+          <Text style={styles.instructions}>
+              Press Cmd+R to reload,{'\n'}
+              Cmd+D or shake for dev menu
+          </Text>
+        </FooterTab>
+      </Footer>
+      </Container>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  // container: {
+  //   flex: 1,
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  //   backgroundColor: '#F5FCFF',
+  // },
+  // welcome: {
+  //   fontSize: 20,
+  //   textAlign: 'center',
+  //   margin: 10,
+  // },
+  // instructions: {
+  //   textAlign: 'center',
+  //   color: '#333333',
+  //   marginBottom: 5,
+  // },
 });
