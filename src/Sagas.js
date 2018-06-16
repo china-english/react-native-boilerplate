fc2261060bd857331c00c84559c2cd5b2c4cfb6f
zhangtong <1263695353@qq.com>
2018-06-16 09:21:06 +0800

    添加redux-saga

---
+++ src/Sagas.js
@@ -0,0 +1,9 @@
+import { take, put, call, fork, select, takeEvery, all } from 'redux-saga/effects'
+
+export function* getAllProducts() {
+  console.log('hello, Welcome to use redux-saga!')
+}
+
+export default function* root() {
+  yield all([fork(getAllProducts)])
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,26 @@
-import rootReducer from '../reducer';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import createSagaMiddleware from 'redux-saga';
+
+import rootReducer from '../reducer';
+import sagas from '../Sagas';
+
 // This connects the reducer to the store
 const configureStore = (preloadedState) => {
+
+  const sagaMIddleware = createSagaMiddleware();
+
   const store = createStore(
     rootReducer,
     preloadedState,
     composeWithDevTools(
-    		applyMiddleware(thunk)
+    		applyMiddleware(sagaMIddleware,thunk)
     	)
   )
+
+  // run saga
+  sagaMIddleware.run(sagas);
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
