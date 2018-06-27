f35b581e602bb9b7028f03b4f2b132e923a471a3
zhangtong <1263695353@qq.com>
2018-06-27 17:14:01 +0800

    test eslint

--- src/container/Login/index.js
+++ src/container/Login/index.js
@@ -4,60 +4,59 @@
  * @flow
  */
 
-import React, { Component } from "react";
-import { Text, View } from "react-native";
-import { Actions } from "react-native-router-flux";
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { Actions } from 'react-native-router-flux';
 
 import {
-    Container,
-    Footer,
-    FooterTab,
-    Header,
-    Button,
-    Content
-} from "native-base";
+  Container,
+  Footer,
+  FooterTab,
+  Header,
+  Button,
+  Content,
+} from 'native-base';
 
-export default class Login extends Component {
-    // eslint-disable-line
-    render() {
-        return (
-            <Container>
-                <Header
-                    style={{
-                        backgroundColor: "#F5FCFF",
-                        justifyContent: "center",
-                        alignItems: "center"
-                    }}
-                >
-                    <Text>Login</Text>
-                </Header>
-                <Content
-                    contentContainerStyle={{
-                        flex: 1,
-                        justifyContent: "center",
-                        alignItems: "center"
-                    }}
-                    style={{
-                        backgroundColor: "#F5FCFF"
-                    }}
-                >
-                    <View>
-                        <Button onPress={() => Actions.pop()}>
-                            <Text>Back Home</Text>
-                        </Button>
-                    </View>
-                </Content>
-                <Footer>
-                    <FooterTab
-                        style={{
-                            justifyContent: "center",
-                            alignItems: "center"
-                        }}
-                    >
-                        <Text>Login footer</Text>
-                    </FooterTab>
-                </Footer>
-            </Container>
-        );
-    }
+export default class Login extends Component {// eslint-disable-line
+  render() {
+    return (
+      <Container>
+        <Header
+          style={{
+            backgroundColor: '#F5FCFF',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Text>Login</Text>
+        </Header>
+        <Content
+          contentContainerStyle={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+          style={{
+            backgroundColor: '#F5FCFF',
+          }}
+        >
+          <View>
+            <Button onPress={() => Actions.pop()}>
+              <Text>Back Home</Text>
+            </Button>
+          </View>
+        </Content>
+        <Footer>
+          <FooterTab
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Text>Login footer</Text>
+          </FooterTab>
+        </Footer>
+      </Container>
+    );
+  }
 }
