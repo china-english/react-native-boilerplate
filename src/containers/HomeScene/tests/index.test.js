c7561250b6655bca17a93c905f7d1da0c205d10d
zhangtong <1263695353@qq.com>
2018-07-02 10:05:21 +0800

    删除test.js相关文件，暂时不需要测试（PS:不想写）

--- src/containers/HomeScene/tests/index.test.js
+++
@@ -1,5 +0,0 @@
-// describe('<HomeScene />', () => {
-//   it('Expect to have unit tests specified', () => {
-//     expect(true).toEqual(false);
-//   });
-// });
--- src/containers/HomeScene/tests/reducer.test.js
+++
@@ -1,9 +0,0 @@
-//
-// import { fromJS } from 'immutable';
-// import homeSceneReducer from '../reducer';
-//
-// describe('homeSceneReducer', () => {
-//   it('returns the initial state', () => {
-//     expect(homeSceneReducer(undefined, {})).toEqual(fromJS({}));
-//   });
-// });
