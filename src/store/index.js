9c1fe3a247778b0c030fcb80391ea8e35762d181
zhangtong <1263695353@qq.com>
2018-07-10 15:23:20 +0800

    移除redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,4 @@
 import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 
@@ -13,7 +12,7 @@ const configureStore = () => {
   const store = createStore(
     createReducer(),
     composeWithDevTools(
-      applyMiddleware(sagaMiddleware, thunk),
+      applyMiddleware(sagaMiddleware),
     ),
   );
 
