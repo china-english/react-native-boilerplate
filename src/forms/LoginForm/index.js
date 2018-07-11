808ed039da7f744db18aa45520b07ca6961cf7e8
zhangtong <1263695353@qq.com>
2018-07-11 08:59:04 +0800

    使用 generate 重新生成 LoginForm

--- src/forms/LoginForm/index.js
+++ src/forms/LoginForm/index.js
@@ -1,62 +1,71 @@
+/**
+*
+* LoginForm
+*
+*/
+
 import React from 'react';
 import PropTypes from 'prop-types';
+import pick from 'lodash/pick';
+import { reduxForm } from 'redux-form/immutable';
 import {
   Button,
   Text,
   Form,
 } from 'native-base';
 
-import pick from 'lodash/pick';
-import { reduxForm } from 'redux-form/immutable';
-
+import { translate } from '../../utils/helpers';
 import Group from '../formFields';
 
 import styles from './styles';
 
-const formFieldsObject = {
-  email: {
-    label: 'Email',
-    type: 'textInput',
-    placeholder: 'placeholderEmail',
-  },
-  password: {
-    label: 'password',
-    type: 'textInput',
-    secureTextEntry: true,
-    placeholder: 'placeholderPassword',
-  },
-};
-
-
 const LoginForm = (props) => {
-  const {
-    handleSubmit, ...otherProps
-  } = props;
+  const { handleSubmit, ...otherProps } = props;
 
-  const loginFileds = [
+  // Add your formField in this object
+  const formFieldsObject = {
+    email: {
+      label: 'Email',
+      type: 'textInput',
+      placeholder: 'placeholderEmail',
+    },
+    password: {
+      label: 'password',
+      type: 'textInput',
+      secureTextEntry: true,
+      placeholder: 'placeholderPassword',
+    },
+  };
+
+  // 使用 pick 方法挑选出你需要展示的formField
+  const formFields = [
     pick(formFieldsObject, 'email', 'password'),
   ];
+
   return (
     <Form style={styles.form}>
-      {loginFileds.map((loginFiled) => (
+      {formFields.map((formField) => (
         <Group
-          fieldsObject={loginFiled}
-          key={loginFiled}
+          fieldsObject={formField}
+          key={formField}
           {...otherProps}
         />
       ))}
       <Button block primary style={styles.button} onPress={handleSubmit}>
-        <Text>Submit</Text>
+        <Text>{translate('submit')}</Text>
       </Button>
     </Form>
   );
 };
+
 LoginForm.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
 };
+
 LoginForm.defaultProps = {
 };
 
 export default reduxForm({
   form: 'loginForm',
+  destroyOnUnmount: false,
 })(LoginForm);
---
+++ src/forms/LoginForm/index.stories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react-native';
+// import { action } from '@storybook/addon-actions';
+// import { linkTo } from '@storybook/addon-links';
+
+import ContentView from '../../../storybook/ContentView';
+
+import LoginForm from '.';
+
+storiesOf('Login Form', module)
+  .addDecorator((getStory) => <ContentView>{getStory()}</ContentView>)
+  .add('LoginForm', () => (
+    <LoginForm />
+  ));
