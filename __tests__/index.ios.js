fa4e50a29d5e28096fde2a2e770a0296e709ef87
zhangtong <1263695353@qq.com>
2018-07-12 17:03:34 +0800

    修改 test 文件， 使其可以正常运行；next => 模版中添加 test 文件

--- __tests__/index.ios.js
+++ __tests__/index.ios.js
@@ -1,12 +1,13 @@
 import 'react-native';
 import React from 'react';
-import Index from '../index.ios.js';
+import renderer from 'react-test-renderer';
+import AppFooter from '../src/components/AppFooter';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   const tree = renderer.create(
-    <Index />
+    <AppFooter />
   );
+  expect(tree).toBeDefined();
 });
--- index.ios.js
+++ index.ios.js
@@ -7,5 +7,5 @@
 import { AppRegistry, YellowBox } from 'react-native';
 import App from './src/App';
 
-YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader', 'Class RCTCxxModule']);
 AppRegistry.registerComponent('nativeBoilerplate', () => App);
