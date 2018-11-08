4377be1bd9d03ec9ac17ac2321c255455853dcb9
zhangtong <1263695353@qq.com>
2018-11-08 21:28:00 +0800

    修改翻译

--- src/translations/zh.js
+++ src/translations/zh.js
@@ -1,17 +1,28 @@
 export default {
+  // generators 生成文件时所需翻译字段
   generatorMessage: '此页面由生成器生成',
   componentMessage: '此组件由生成器生成',
   welcome: '欢迎使用生成器生成你的文件',
+
+  // LoginScene
+  loginScene: '登录',
   submit: '提交',
   placeholderEmail: '请输入你的邮件',
   placeholderPassword: '请输入你的密码',
   email: '邮箱',
   password: '密码',
+
+  // HomeScene
+  homeScene: '首页',
   login: '登录',
   zh: '中文',
   en: 'English',
   add: '加',
   minus: '减',
+
+  // AppFooter
   footer: 'native 模版',
+
+  // AppHeader
   back: '返回',
 };
